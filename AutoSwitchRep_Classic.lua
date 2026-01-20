4e536caf9422c69f07f0eeb1a943d5d0ac1263ee
gromit <gromit8209@gmail.com>
2026-01-21 02:06:17 +0900

    최신화

--- AutoSwitchRep_Classic.lua
+++ AutoSwitchRep_Classic.lua
@@ -1,54 +1,61 @@
-local AutoSwitchRep = CreateFrame'Frame'
+local AutoSwitchRep = CreateFrame("Frame")
 
-AutoSwitchRep:RegisterEvent'CHAT_MSG_COMBAT_FACTION_CHANGE'
-AutoSwitchRep:RegisterEvent'PLAYER_LOGIN'
+-- 이벤트 등록
+AutoSwitchRep:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE")
+AutoSwitchRep:RegisterEvent("PLAYER_LOGIN")
 
-AutoSwitchRep:SetScript('OnEvent', function(self, event, ...)
-  self[event](self, event, ...)
+-- 패턴 미리 생성 (성능 최적화)
+local pattern_inc = FACTION_STANDING_INCREASED:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)")
+local pattern_dec = FACTION_STANDING_DECREASED:gsub("%%s", "(.+)"):gsub("%%d", "(%%d+)")
+local pattern_gen = FACTION_STANDING_INCREASED_GENERIC:gsub("%%s", "(.+)")
+
+-- 평판 이름을 ID로 매핑할 테이블
+local factionNameToID = {}
+
+-- 평판 목록 갱신 함수
+local function UpdateFactionList()
+    wipe(factionNameToID)
+    for i = 1, GetNumFactions() do
+        local name, _, _, _, _, _, _, _, isHeader, _, _, _, _, factionID = GetFactionInfo(i)
+        if name and not isHeader and factionID then
+            factionNameToID[name] = i
+        end
+    end
+end
+
+AutoSwitchRep:SetScript("OnEvent", function(self, event, ...)
+    if event == "PLAYER_LOGIN" then
+        UpdateFactionList()
+        
+    elseif event == "CHAT_MSG_COMBAT_FACTION_CHANGE" then
+        local msg = ...
+        local factionName
+        
+        -- 메시지에서 평판 이름 추출
+        local f, a = msg:match(pattern_inc)
+        if not f then f, a = msg:match(pattern_dec) end
+        if not f then f = msg:match(pattern_gen) end
+        
+        factionName = f
+        
+        if factionName and factionNameToID[factionName] then
+            local i = factionNameToID[factionName]
+            
+            -- 현재 추적 중인 평판 데이터 가져오기 (최신 API)
+            local watchedData = C_Reputation.GetWatchedFactionData()
+            local currentName = watchedData and watchedData.name
+            
+            -- 현재 추적 중인 평판과 다를 때만 교체
+            if currentName ~= factionName then
+                SetWatchedFactionIndex(i)
+                
+                -- 메시지 출력 (autorep_string이 없을 경우를 대비한 방어 코드)
+                local outputMsg = (autorep_string and autorep_string["CHANGE_REPUTATION_BAR"]) or "평판 바 전환: %s"
+                -- print(format("|cff99CCFF" .. outputMsg .. "|r", factionName))
+            end
+        else
+            -- 목록에 없는 새로운 평판일 수 있으므로 갱신 후 재시도
+            UpdateFactionList()
+        end
+    end
 end)
-
-local pattern_standing_inc = string.gsub(string.gsub(FACTION_STANDING_INCREASED, "(%%s)", "(.+)"), "(%%d)", "(%%d+)")
-local pattern_standing_dec = string.gsub(string.gsub(FACTION_STANDING_DECREASED, "(%%s)", "(.+)"), "(%%d)", "(%%d+)")
-local pattern_standing_inc_generic = string.gsub(FACTION_STANDING_INCREASED_GENERIC, "(%%s)", "(.+)")
-local factions = {}
-
-AutoSwitchRep.PLAYER_LOGIN = function(...)
-  factions = {}
-
-  for i = 1, GetNumFactions() do
-    local name, _, _, _, _, _, _, _, isHeader, isCollapsed, _, _, _, factionID, _, _ = GetFactionInfo(i)
-    if isHeader == false then
-      factions[name] = i
-    end
-  end
-
-  --for k,v in pairs(factions) do print(k .. " : " .. v) end
-end
-
-AutoSwitchRep.CHAT_MSG_COMBAT_FACTION_CHANGE = function(...)
-  local _, _, arg1 = ...
-
-  -- Check standing message for faction increase or decrease
-  local s1, e1, faction, amount = string.find(arg1, pattern_standing_inc)
-  -- If increase not found check for decrease
-  if (s1 == nil) then
-    s1, e1, faction, amount = string.find(arg1, pattern_standing_dec)
-    if (s1 ~= nil) then
-        dec = true
-    end
-  end
-  -- check for generic gain
-  if (s1 == nil) then
-    s1, e1, faction = string.find(arg1, pattern_standing_inc_generic)
-  end
-
-  if (s1 ~= nil) then
-    local _, _, _, _, _, currentlyWatchedID = GetWatchedFactionInfo()
-    local name, _, _, _, _, _, _, _, isHeader, isCollapsed, _, _, _, factionID, _, _ = GetFactionInfo(factions[faction])
-    --print("currentlyWatchedID : " .. currentlyWatchedID .. "factionID : " ..factionID)
-    if (currentlyWatchedID ~= factionID) then
-      SetWatchedFactionIndex(factions[faction])
-      print(format(autorep_string["CHANGE_REPUTATION_BAR"], faction))
-    end
-  end
-end
